4597b66973fd0df9632953e962606df54d0f4f32
Nhrkhsnh <140361183+Nhrkhsnh@users.noreply.github.com>
2023-11-17 16:21:41 +0700

    Add files via upload

---
+++ src/volume/prisma.py
@@ -0,0 +1,6 @@
+def volume_prisma(alas_segitiga, tinggi_segitiga, tinggi_prisma):
+ return (0.5 * tinggi_segitiga * alas_segitiga * tinggi_prisma)
+
+ from alas_segitiga import alas_segitiga as As
+ from tinggi_segitiga import tinggi_segitiga as Ts
+ from tinggi_prisma import tinggi_prisma as Tp
